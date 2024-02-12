c4fce04733477c422f823f8061bc20eb9ad942a0
StephGarage404 <stephane.1995@hotmail.fr>
2024-02-12 11:08:05 +0100

    steph

---
+++ partials/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP-POO-ZOO</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body class="bg-gradient-to-r from-yellow-500 via-yellow-800 to-yellow-800 h-screen" onload="loader()">
+    
+</body>
+</html>
