2bd2f859a5906a8e43c477386ca07b1f4c3c1c38
Jorge Robles <jorgejoanrobles@gmail.com>
2024-02-12 09:57:47 +0100

    squelette projet

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+include './partials/header.php';
+
+?>
+
+<div class="grid place-items-center h-screen">
+    <div class="max-w-lg mx-auto">
+      <img src="./images/zoo.png" class="w-200" alt="" srcset="">
