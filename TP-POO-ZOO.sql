2a8462e3a099c74e1ea00917da0af2773baf2384
Jorge Robles <jorgejoanrobles@gmail.com>
2024-02-14 09:10:15 +0100

    jorges changements

---
+++ TP-POO-ZOO.sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql
+-- Generation Time: Feb 13, 2024 at 03:28 PM
+-- Server version: 10.6.12-MariaDB-1:10.6.12+maria~ubu2004-log
+-- PHP Version: 8.1.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `TP-POO-ZOO`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Animals`
+--
+
+CREATE TABLE `Animals` (
+  `id` int(11) NOT NULL,
+  `namespecies` varchar(11) NOT NULL,
+  `weight` int(11) NOT NULL,
+  `size` int(11) NOT NULL,
+  `age` int(11) NOT NULL,
+  `health` int(11) NOT NULL,
+  `isSleeping` tinyint(1) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `enclos_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Employee`
+--
+
+CREATE TABLE `Employee` (
+  `id` int(11) NOT NULL,
+  `Name` int(11) NOT NULL,
+  `Age` int(11) NOT NULL,
+  `Gender` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Enclos`
+--
+
+CREATE TABLE `Enclos` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
+  `numberofanimals` int(11) NOT NULL,
+  `zoo_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL,
+  `username` int(11) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Animals`
+--
+ALTER TABLE `Animals`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Employee`
+--
+ALTER TABLE `Employee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Enclos`
+--
+ALTER TABLE `Enclos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Animals`
+--
+ALTER TABLE `Animals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Employee`
+--
+ALTER TABLE `Employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Enclos`
+--
+ALTER TABLE `Enclos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `User`
+--
+ALTER TABLE `User`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
